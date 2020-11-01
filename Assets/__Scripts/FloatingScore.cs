e0f2aaf7b2836b09ee64af15013c5e49b2dade20
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-10-31 21:28:50 -0400

    imported previous week's work
    check the prospector bracnh for details

--- Assets/__Scripts/FloatingScore.cs
+++ Assets/__Scripts/FloatingScore.cs
@@ -3,141 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// An enum to track the possible states of a FloatingScore
 public enum eFSState
 {
-    idle,
-    pre,
-    active,
-    post
+    idle, pre, active, post
 }
-
-public class FloatingScore : MonoBehaviour {
-    [Header("Set Dynamically")]
+public class FloatingScore : MonoBehaviour
+{
+    [Header("set dynamically")]
     public eFSState state = eFSState.idle;
 
-    [SerializeField]
-    protected int _score = 0;
+    [SerializeField]protected int _score = 0;
     public string scoreString;
 
-    // The score property sets both _score and scoreString
     public int score
     {
-        get
-        {
-            return (_score);
+        get {
+            return(_score);
         }
-        set
-        {
+        set {
             _score = value;
-            scoreString = _score.ToString("N0"); // "N0 adds commas to the num
-            // Search "C# Standard Numeric Format Strings" for ToString formats
+            scoreString = _score.ToString("N0");
             GetComponent<Text>().text = scoreString;
         }
     }
 
-    public List<Vector2> bezierPts; // Bezier points for movement
-    public List<float> fontSizes; // Bezier points for font scaling
+    public List<Vector2> bezierPts;
+    public List<float> fontSizes;
     public float timeStart = -1f;
     public float timeDuration = 1f;
-    public string easingCurve = Easing.InOut; // Uses Easing in Utils.cs
+    public string easingCurve = Easing.InOut;
 
-    // The GameObject that will receive the SendMessage when this is done moving
     public GameObject reportFinishTo = null;
 
-    private RectTransform rectTrans;
+    private RectTransform rectTransform;
     private Text txt;
 
-    // Set up the FloatingScore and movement
-    // Note the use of parameter defaults for eTimeS & eTimeD
-    public void Init(List<Vector2> ePts, float eTimeS = 0, float eTimeD = 1)
+    public void Init(List<Vector2> ePts, float eTimeS=0, float eTimeD = 1)
     {
-        rectTrans = GetComponent<RectTransform>();
-        rectTrans.anchoredPosition = Vector2.zero;
-
+        rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = Vector2.zero;
+        //print(txt);
         txt = GetComponent<Text>();
-
+        //print(txt);
         bezierPts = new List<Vector2>(ePts);
 
-        if (ePts.Count == 1)
+        if(ePts.Count ==1)
         {
-            // If there's only one point
-            // ... then just go there.
             transform.position = ePts[0];
             return;
         }
 
-        // If eTimeS is the default, just start at the current time
-        if (eTimeS == 0) eTimeS = Time.time;
+        if(eTimeS ==0)eTimeS=Time.time;
         timeStart = eTimeS;
         timeDuration = eTimeD;
 
-        state = eFSState.pre; // Set it to the pre state, ready to start moving
+        state=eFSState.pre;
     }
 
     public void FSCallback(FloatingScore fs)
     {
-        // When this callback is called by SendMessage,
-        // add the score from the calling FloatingScore
-        score += fs.score;
+        score+= fs.score;
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        // If this is not moving, just return
-        if (state == eFSState.idle) return;
+        if(state==eFSState.idle)return;
 
-        // Get u from the current time and duration
-        // u ranges from 0 to 1 (usually)
-        float u = (Time.time - timeStart) / timeDuration;
-        //Use Easing class from Utils to curve the u value
+        float u = (Time.time - timeStart)/timeDuration;
+
         float uC = Easing.Ease(u, easingCurve);
-        if (u < 0)
+        if(u<0)
         {
-            // If u<0, then we shouldn't move yet.
             state = eFSState.pre;
-            txt.enabled = false; // Hide the score initially
-        }
-        else
+            txt.enabled = false;
+
+        } else
         {
-            if (u >= 1)
+            if(u>=1)
             {
-                // If u>=1, we're done moving
-                uC = 1; // Set uC=1 so we don't overshoot
+                uC=1;
                 state = eFSState.post;
-                if (reportFinishTo != null)
+                if(reportFinishTo!=null)
                 {
-                    // If there's a callback GameObject
-                    // Use SendMessage to call the FSCallback method
-                    // with this as the parameter.
                     reportFinishTo.SendMessage("FSCallback", this);
-                    // Now that the message has been sent,
-                    // Destroy this gameObject
                     Destroy(gameObject);
-                }
-                else
+                } else
                 {
-                    // If there is nothing to callback
-                    // ...then don't destroy this. Just let it stay still.
-                    state = eFSState.idle;
+                    state=eFSState.idle;
                 }
-            }
-            else
+            } else
             {
-                // o<=u<1, which means that this is active and moving
                 state = eFSState.active;
-                txt.enabled = true; // Show the score once more
+                txt.enabled = true;
             }
-            // Use Bezier curve to move this to the right point
             Vector2 pos = Utils.Bezier(uC, bezierPts);
-            // RectTransform anchors can be used to position UI objects relative
-            // to total size of the screen
-            rectTrans.anchorMin = rectTrans.anchorMax = pos;
-            if (fontSizes != null && fontSizes.Count > 0)
+            rectTransform.anchorMin = rectTransform.anchorMax = pos;
+            if(fontSizes != null && fontSizes.Count>0)
             {
-                // If fontSizes has values in it
-                // ...then adjust the fontSize of this GUIText
                 int size = Mathf.RoundToInt(Utils.Bezier(uC, fontSizes));
                 GetComponent<Text>().fontSize = size;
             }
--- Assets/__Scripts/Layout.cs
+++ Assets/__Scripts/Layout.cs
@@ -2,92 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//The SlotDef class is not a subclass of MonoBehaviour, so it doesn't need
-//a separate C# file.
-[System.Serializable] // This makes SlotDefs visible in the Unity Inspector pane
-public class SlotDef
+//[System.Serializable]
+/*public class SlotDef
 {
     public float x;
     public float y;
-    public bool faceUp = false;
-    public string layerName = "Default";
+    public bool faceUp=false;
+    public string  layerName="Default";
     public int layerID = 0;
     public int id;
     public List<int> hiddenBy = new List<int>();
     public string type = "slot";
     public Vector2 stagger;
-}
-
-public class Layout : MonoBehaviour {
-    public PT_XMLReader xmlr; // Just like Deck, this has a PT_XMLReader
-    public PT_XMLHashtable xml; // This variable is for faster xml access
-    public Vector2 multiplier; // The offset of the tableau's center
-    //SlotDef references
-    public List<SlotDef> slotDefs; // All the SlotDefs for Row0-Row3
+}*/
+public class Layout : MonoBehaviour
+{
+    public PT_XMLReader xmlr;
+    public PT_XMLHashtable xml;
+    public Vector2 multiplier;
+    public List<SlotDef> slotDefs;
     public SlotDef drawPile;
     public SlotDef discardPile;
-    // This holds all of the possible names for the layers set by layerID
-    public string[] sortingLayerNames = new string[] { "Row0", "Row1", "Row2", "Row3", "Discard", "Draw" };
+    public SlotDef trayPile;
 
-	// This function is called to read in the LayoutXML.xml file
+    public string[] sortingLayerNames =new string[]{"Row0", "Row1","Row2","Row3","Discard","Draw","Row4","Row5","Trays"};
+    /*private void Awake()
+    {
+        sortingLayerNames = new string[9];
+        sortingLayerNames[0]= "Row0";
+        sortingLayerNames[1]="Row1";
+        sortingLayerNames[2]="Row2";
+        sortingLayerNames[3]= "Row3";
+        sortingLayerNames[4]= "Discard";
+        sortingLayerNames[5] = "Draw";
+        sortingLayerNames[6] = "Row4";
+        sortingLayerNames[7] = "Row5";
+        sortingLayerNames[8] ="Trays";
+    }*/
     public void ReadLayout(string xmlText)
     {
         xmlr = new PT_XMLReader();
-        xmlr.Parse(xmlText); // The XML is parsed
-        xml = xmlr.xml["xml"][0]; // And xml is set as a shortcut to the XML
+        xmlr.Parse(xmlText);
+        xml = xmlr.xml["xml"][0];
 
-        //Read in the multiplier, which sets card spacing
         multiplier.x = float.Parse(xml["multiplier"][0].att("x"));
         multiplier.y = float.Parse(xml["multiplier"][0].att("y"));
 
-        //Read in the slots
         SlotDef tSD;
-        //slotsX is used as a shortcut to all the <slot>s
         PT_XMLHashList slotsX = xml["slot"];
 
-        for (int i=0; i<slotsX.Count; i++)
+        for(int i = 0; i<slotsX.Count; i++)
         {
-            tSD = new SlotDef(); // Create a new SlotDef instance
-            if (slotsX[i].HasAtt("type"))
+            tSD = new SlotDef();
+            if(slotsX[i].HasAtt("type"))
             {
-                // If this <slot> has a type attribute parse it
                 tSD.type = slotsX[i].att("type");
-            }
-            else
+            } else
             {
-                // If not, set its type to "slot"; it's a card in the rows
                 tSD.type = "slot";
             }
-            // Various attributes are parsed into numerical values
             tSD.x = float.Parse(slotsX[i].att("x"));
             tSD.y = float.Parse(slotsX[i].att("y"));
             tSD.layerID = int.Parse(slotsX[i].att("layer"));
-            //This converts the number of the layerID into a text layerName
+
+            //print(sortingLayerNames[5]);
             tSD.layerName = sortingLayerNames[tSD.layerID];
 
-            switch (tSD.type)
+            switch(tSD.type)
             {
-                //pull additional attributes based on the type of this <slot>
-                case "slot":
-                    tSD.faceUp = (slotsX[i].att("faceup") == "1");
+                case"slot":
+                    tSD.faceUp = (slotsX[i].att("faceup")=="1");
                     tSD.id = int.Parse(slotsX[i].att("id"));
-                    if (slotsX[i].HasAtt("hiddenby"))
+                    if(slotsX[i].HasAtt("hiddenby"))
                     {
                         string[] hiding = slotsX[i].att("hiddenby").Split(',');
-                        foreach (string s in hiding)
+                        foreach(string s in hiding)
                         {
                             tSD.hiddenBy.Add(int.Parse(s));
+
                         }
+
                     }
                     slotDefs.Add(tSD);
                     break;
 
-                case "drawpile":
+                case"drawpile":
                     tSD.stagger.x = float.Parse(slotsX[i].att("xstagger"));
-                    drawPile = tSD;
+                    drawPile =tSD;
                     break;
-                case "discardpile":
-                    discardPile = tSD;
+
+                case"discardpile":
+                    discardPile =tSD;
+                    break;
+                case"traypile":
+                    trayPile =tSD;
                     break;
             }
         }
