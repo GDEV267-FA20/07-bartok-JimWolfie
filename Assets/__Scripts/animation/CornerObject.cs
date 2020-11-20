e8bc503b5f1ab0d66cb8743ca4bdfafcd1e2f1b0
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2020-11-19 21:41:34 -0500

    teest

--- Assets/__Scripts/animation/CornerObject.cs
+++ Assets/__Scripts/animation/CornerObject.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CornerObject : MonoBehaviour
+[CreateAssetMenu(fileName = "cornerobject", menuName = "cornerObjects/newObject", order = 2)]
+public class CornerObject : ScriptableObject
 {
-   
+    public Color color{get{return sprite.color;}set{sprite.color = value; } }
+    readonly public Vector3 pos;
+    public SpriteRenderer sprite;
+
 }
--- Assets/__Scripts/animation/PassedObject.cs
+++ Assets/__Scripts/animation/PassedObject.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PassedObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool runningAlgo = true;
+    public Color currentColor;
+    public ScriptableObject tpR;
+    public ScriptableObject tpL;
+    public ScriptableObject btR;
+    public ScriptableObject btL;
+
+    public Color topRightColor{get {return (tpR.color); } set{ tpR.color = value;}} 
+    public Color topLeftColor { get { return (tpL.color); } set { tpL.color = value; } } 
+    public Color bottomRightColor { get { return (btR.color); } set { btR.color = value; } } 
+    public Color bottomLeftColor { get { return (btL.color); } set { btL.color = value; } } 
+    public List<Vector4> AllColors;
+    public bool AllcolorsMatch()
+    {
+        if(isSameColor(topLeftColor, topRightColor) || isSameColor(topLeftColor, bottomLeftColor)||isSameColor(topLeftColor, bottomRightColor) == false)
+            {
+            return(false);
+        }
+        if(isSameColor(topRightColor, bottomRightColor)||isSameColor(topRightColor, bottomLeftColor) == false)
+        {
+            return(false);
+        }
+        if(isSameColor(bottomRightColor, bottomLeftColor)== false)
+        {
+            return(false);
+        }
+        return (false); 
+    }
+    public bool isSameColor(Vector4 colorA, Vector4 colorB)
     {
         
+        return (colorA.Equals(colorB));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(AllcolorsMatch())
+        {
+            //end the thing and stop updating
+        } 
     }
 }
